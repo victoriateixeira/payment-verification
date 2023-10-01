67307c5ad399af34749017865d884dc491a6958c
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-10-01 20:22:32 -0300

    feat: add Luhn's algorithm check for card PAN

--- backend/src/PaymentService.js
+++ backend/src/PaymentService.js
@@ -17,7 +17,10 @@ const validateDetails = (month, year, cvv, pan) => {
   const valPan = validatePan(pan);
   if (valPan.type) return valPan;
 
-  return {type: 200, message: 'Success! Card information is valid.'}
+  const checkLastDigit = checkDigit(pan);
+  if(!checkLastDigit) return {type: 400, message: 'PAN not valid'}
+
+  return {type: 200, message: 'Card information is valid.'}
   
 }
 
@@ -25,7 +28,7 @@ const validateDetails = (month, year, cvv, pan) => {
 
 const validateDate = (month, year) => {
 const currDateYear = new Date().getFullYear()
-const currDateMonth = new Date().getMonth()
+const currDateMonth = new Date().getMonth() + 1
 if ((currDateYear > Number(year)) || ((currDateYear === Number(year)) && currDateMonth > Number(month)) ){
   return {type: 400, message: 'Card expiry date is not valid'}
 }
@@ -55,6 +58,28 @@ if( pan.length >= 16 && pan.length <= 19) {
   return {type: 400, message: 'PAN not valid'}
 }
 
+const checkDigit = (pan) => {
+ 
+    let sum = 0;
+    let isEven = false;
+  
+    for (let i = pan.length-1; i >=0; i-=1) {
+      let digit = parseInt(pan.charAt(i), 10);
+  
+      if (isEven) {
+        digit *= 2;
+        if (digit > 9) {
+          digit -= 9;
+        }
+      }
+  
+      sum += digit;
+      isEven = !isEven;
+    }
+    return (sum % 10) === 0;
+  }
+ 
+  
 
 
 module.exports = {validateDetails}
