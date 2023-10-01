45757acd52638b1326a4e08c678808a1cb9e79d2
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-10-01 20:19:42 -0300

    refactor: changes state structure on FormPage

--- frontend/src/pages/FormPage.jsx
+++ frontend/src/pages/FormPage.jsx
@@ -1,97 +1,138 @@
 import { useState } from 'react';
 import React from 'react';
-
+import Cards from "react-credit-cards-2";
+import "react-credit-cards-2/dist/es/styles-compiled.css";
+import './FormPage.css';
 import { postAPI } from '../services/cardAPI';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons'
 
 function FormPage() {
 
-  const [cardNumber, setCardNumber] = useState('');
-  const [expiryMonth, setExpiryMonth] = useState('');
-  const [expiryYear, setExpiryYear] = useState('');
-  const [cvv, setCvv] = useState('');
+  // const [cardNumber, setCardNumber] = useState('');
+  // const [expiryMonth, setExpiryMonth] = useState('');
+  // const [expiryYear, setExpiryYear] = useState('');
+  // const [cvv, setCvv] = useState('');
   const [response, setResponse] = useState('');
-  const [invalidCard, setInvalidCard] = useState();
+  const [invalidCard, setInvalidCard] = useState(false);
+  const [validCard, setValidCard] = useState(false);
 
+  const [cardData, setCardData] = useState({
+    pan: '',
+    expiry: '',
+    cvv:'',
+  })
+
+   const onInputChange = (event) => {
+    const { name, value } = event.target;
+    setCardData({ ...cardData, [name]: value });
+  };
+ const onCloseButton = () => {
+  setInvalidCard(false);
+  setValidCard(false)
+ }
 const checkCardInfo = async (e) => {
   e.preventDefault();
+  console.log('OIIIII')
   const obj = {
-    pan: cardNumber,
-    cvv: cvv,
-    month: expiryMonth,
-    year: expiryYear
+    pan: cardData.pan,
+    cvv: cardData.cvv,
+    month: cardData.expiry.slice(0,2),
+    year: cardData.expiry.slice(3,7),
   }
+  console.log(obj)
   const resp = await postAPI(obj);
   console.log(resp)
   setResponse(resp.message)
   if(resp.type === 400) {
     setInvalidCard(true)
+    setValidCard(false)
+  }
+  if(resp.type === 200) {
+    setValidCard(true)
+    setInvalidCard(false)
   }
 }
 
   return (
     <main>
+      <Cards
+       number={cardData.pan}
+       expiry={cardData.expiry}
+       cvc={cardData.cvv}
+       name='JOHN SMITH'
+      //  focused={state.focus}
+      />
       <form>
-        <label htmlFor="cardNumber">
+        <label htmlFor="pan">
           <input 
-          id='cardNumber'
+          id='pan'
           type="text" 
-          name="cardNumber"
-          value={cardNumber}
-          placeholder='card number'
-          onChange={(e) => setCardNumber(e.target.value)}
+          name="pan"
+          value={cardData.pan}
+          placeholder='PAN'
+          onChange={onInputChange}
           />
         </label>
-        <label htmlFor="expiryMonth">
+        <label htmlFor="expiry">
         <input 
-          id='expiryMonth'
+          id='expiry'
           type="text" 
-          name="expiryMonth"
-          value={expiryMonth}
-          minLength={2}
-          maxLength={2}
-          placeholder='MM'
-          onChange={(e) => setExpiryMonth(e.target.value)}
+          name="expiry"
+          value={cardData.expiry}
+          minLength={7}
+          maxLength={7}
+          placeholder='MM/YYYY'
+          onChange={onInputChange}
           />
         </label>
-        <label htmlFor="expiryYear">
-        <input 
-          id='expiryYear'
-          type="text" 
-          name="expiryYear"
-          value={expiryYear}
-          minLength={4}
-          maxLength={4}
-          placeholder='YYYY'
-          onChange={(e) => setExpiryYear(e.target.value)}
-          />
-        </label>
+
         <label htmlFor="cvv">
         <input 
           id='cvv'
           type="text" 
           name="cvv"
-          value={cvv}
+          value={cardData.cvv}
           placeholder='CVV'
-          onChange={(e) => setCvv(e.target.value)}
+          onChange={onInputChange}
           />
         </label>
 
         <button
         type='submit'
+        className='submit-button'
         onClick={checkCardInfo}       >
-          Submit
+          CHECK INFO
         </button>
       </form>
-      {/* {invalidCard && (
-        <div data-testid="common_login__element-invalid-email">
-          Login Inválido
-        </div>
-      )} */}
-      { response && (
-        <div> {response}</div>
-      )}
+      
+      <div className='content'>
+      {validCard && (
+        <div className="alert alert-success alert-white rounded">
+         <button type="button" className="close"  onClick={onCloseButton}>×</button>
+          <div className="icon"> 
+           <i>
+            <FontAwesomeIcon icon={faCheck} style={{color: "#ffffff",}} />
+           </i>
+          </div>
+        <strong>Success!</strong> {response}
+       </div>
+       )}
+       {invalidCard && (
+       <div className="alert alert-danger alert-white rounded">
+         <button type="button" className="close"  onClick={onCloseButton}>×</button>
+         <div className="icon">
+          <i>
+           <FontAwesomeIcon icon={faCircleXmark} style={{color: "#ffffff",}} />
+          </i>
+         </div>
+         <strong>Error!</strong> {response}.
+       </div>
+       )}
+      </div>
     </main>
   )
-    }
+    }   
 
 export default FormPage;
