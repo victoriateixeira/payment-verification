9c4a185bf4792f8a5c4c65b692d00ebfc174f92f
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-10-02 15:26:40 -0300

    fix: adds cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors({origin: 'http://localhost:3000'}));
+
 const paymentController = require('./PaymentController');
 
 app.post('/verification', paymentController.validateDetails);
